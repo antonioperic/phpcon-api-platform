b2a8c9e86e36fa3a20eb4aa03265b5daff951272
Antonio Peric-Mazar <antonio@locastic.com>
2019-11-11 21:18:45 +0100

    added presentation entity

---
+++ api/src/Entity/Presentation.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass="App\Repository\PresentationRepository")
+ */
+class Presentation
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $startsAt;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Event")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $event;
+
+    /**
+     * @ORM\Column(type="decimal", precision=10, scale=2)
+     */
+    private $averageRating = 0;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getStartsAt(): ?string
+    {
+        return $this->startsAt;
+    }
+
+    public function setStartsAt(?string $startsAt): self
+    {
+        $this->startsAt = $startsAt;
+
+        return $this;
+    }
+
+    public function getEvent(): ?Event
+    {
+        return $this->event;
+    }
+
+    public function setEvent(?Event $event): self
+    {
+        $this->event = $event;
+
+        return $this;
+    }
+
+    public function getAverageRating(): ?string
+    {
+        return $this->averageRating;
+    }
+
+    public function setAverageRating(string $averageRating): self
+    {
+        $this->averageRating = $averageRating;
+
+        return $this;
+    }
+}
